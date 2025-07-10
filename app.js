535a8739a46b4b274e85aabd62db758536714c6b
maxheerdegen <96018857+maxheerdegen@users.noreply.github.com>
2025-07-10 12:43:34 +0200

    add locig to login component

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ import { workoutRouter } from './src/routes/workoutRoutes.js';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
--- react/src/components/Login/Login.jsx
+++ react/src/components/Login/Login.jsx
@@ -3,6 +3,27 @@ import AuthForm from "../AuthForm/AuthForm.jsx";
 function Login () {
 
     const handleLogin = async (username, password) => {
+        
+        try {
+            const response = await fetch("http://localhost:3000/api/auth/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({username, password}),
+                credentials: "include",
+            })
+    
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+    
+            const result = await response.json();
+    
+            console.log(result);
+        } catch (err) {
+            console.log(err);
+        }
 
     }
 
